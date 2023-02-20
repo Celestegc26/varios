6599aaad5fff196a930751c00bbd73b6cdd817e4
Celestegc26 <yuligonzalez26@gmail.com>
2023-02-20 13:54:38 +0100

    modulares

---
+++ _17_cliente_calculadora_no_modular/src/_17_cliente_calculadora_no_modular/Test.java
@@ -0,0 +1,14 @@
+package _17_cliente_calculadora_no_modular;
+
+import service.Calculadora;
+import utilidades.Operaciones;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Operaciones oper=new Operaciones();
+		Calculadora cal=new Calculadora();
+
+	}
+
+}
